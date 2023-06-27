2942ec82fb94e9bdc3cc0e73fce82b940e1db74c
Harrison Anthony <harrisoneanthony@gmail.com>
2023-06-27 17:39:33 -0400

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Harrison Anthony on 6/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
