22ab05c309949747ae94f35b1532edf917ff04ac
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-01 15:18:28 -0400

    added functionality for adding items into store and removed initializer code for generating 5 random items on load

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -13,7 +13,16 @@ class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func addNewItem(_ sender: UIButton) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
         
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            //Insert this new row into the table
+            tableView.insertRows(at:[indexPath], with: .automatic)
+        }
     }
     
     @IBAction func toggleEditingMode(_ sender: UIButton) {
