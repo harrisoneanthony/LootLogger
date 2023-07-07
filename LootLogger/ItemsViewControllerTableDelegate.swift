ccc086ac747f822646b661bd9d3179d38564ba88
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-06 20:38:25 -0400

    fixing repository issues

---
+++ LootLogger/ItemsViewControllerTableDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  ItemsViewControllerTableDelegate.swift
+//  LootLogger
+//
+//  Created by Harrison Anthony on 7/6/23.
+//
+
+import UIKit
+
+extension ItemsViewController {
+    
+    func setUpTable() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get a new or recycled cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        
+        // set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // if the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // remove the item from the store
+            itemStore.removeItem(item)
+            
+            // also remove that row from the table view with an animation
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // update the model
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+
+}
+
