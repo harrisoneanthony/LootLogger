6639014ee6a43bf654af0db7e6daca772a255205
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-12 13:32:51 -0400

    added property for item instance and override viewWillAppear(_:)

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet var nameField: UITextField!
-    
-    @IBOutlet var serialField: UITextField!
-    
+    @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
+    
+    @IBOutlet var dateLabel: UILabel!
+    var item: Item!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = "\(item.valueInDollars)"
+        dateLabel.text = "\(item.dateCreated)"
+    }
 }
