4ec08764c81739af0ec1eacb351fe166b43bf3da
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-12 11:43:11 -0400

    added DetailViewController and hooked up outlets to UItextfields

---
+++ LootLogger/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  LootLogger
+//
+//  Created by Harrison Anthony on 7/12/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var serialField: UITextField!
+    
+    @IBOutlet var valueField: UITextField!
+}
