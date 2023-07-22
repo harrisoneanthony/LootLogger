707db491f5aabf1811388ab44bd2538c29203c06
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-22 13:49:30 -0400

    adding UINavigationController

--- LootLogger/SceneDelegate.swift
+++ LootLogger/SceneDelegate.swift
@@ -21,7 +21,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let itemStore = ItemStore()
         
         //Access the ItemsViewController and set its item store
-        let itemsController = window!.rootViewController as! ItemsViewController
+        // let itemsController = window!.rootViewController as! ItemsViewController
+        let navController = window!.rootViewController as! UINavigationController
+        let itemsController = navController.topViewController as! ItemsViewController
         itemsController.itemStore = itemStore
     }
 
