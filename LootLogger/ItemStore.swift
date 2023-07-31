9a45c755d5e6bc500b0696c34906e856f0d883e9
Harrison Anthony <harrisoneanthony@gmail.com>
2023-07-31 00:11:50 -0400

    adding functionality for persistent data

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.plist")
+    }()
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -36,6 +42,29 @@ class ItemStore {
         
         // insert item in array at new location
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    @objc func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL)")
+        do {
+            let encoder = PropertyListEncoder()
+            let data =  try encoder.encode(allItems)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all of the items")
+            return true
+        } catch let encodingError {
+            print("Error encoding allItems: \(encodingError)")
+            return false
+        }
+    }
+    
+    init() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
+                                       
     }
 }
 
