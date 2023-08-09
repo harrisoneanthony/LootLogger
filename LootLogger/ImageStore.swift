0b23082714e5acfd2e406e61c66be29b61e4c313
Harrison Anthony <harrisoneanthony@gmail.com>
2023-08-09 13:42:34 -0400

    saving image data to disk (ImageStore.swift)

--- LootLogger/ImageStore.swift
+++ LootLogger/ImageStore.swift
@@ -12,6 +12,15 @@ class ImageStore {
     
     func setImage(_ image: UIImage, forKey key: String){
         cache.setObject(image, forKey: key as NSString)
+        
+        // create full URL for image
+        let url = imageURL(forKey: key)
+        
+        // turn image into JPEG data
+        if let data = image.jpegData(compressionQuality: 0.5){
+            // write it to full URL
+            try? data.write(to: url)            
+        }
     }
     
     func image(forKey key: String) -> UIImage? {
